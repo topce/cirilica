f6f588b8691fa58df50dae83f89cab7ebf5bec7b
topce <slobodan.filipovic@gmail.com>
2022-12-30 19:55:28 +0100

    chatgpt

--- src/lib.rs
+++ src/lib.rs
@@ -11,172 +11,145 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 /// transform utf-8 serbian latinic to serbian cyrilic
 /// also translate two letters nj,lj and dž
-/// and capital Nj,Lj and Dž 
+/// and capital Nj,Lj and Dž
 /// in one cyrilic letter  
-pub fn to_serbian_cyrillic(word: String) -> String {
-    let mut result = String::with_capacity(word.bytes().len() * 2);
-    let mut chars = word.chars();
-    let mut previous_char = '\0';
-    loop {
-        let letter = chars.next();
+pub fn latin_to_cyrillic(input: String) -> String {
+    let mut output = String::new();
+    let mut iter = input.chars().peekable();
 
-        match letter {
-            Some(a) => match a {
-                'n' | 'l' | 'd' | 'N' | 'L' | 'D' => {
-                    if previous_char != '\0' {
-                        result.push(to_serbian_cyrillic_char(previous_char));
+    while let Some(c) = iter.next() {
+        match c {
+            'a' => output.push('а'),
+            'b' => output.push('б'),
+            'c' => output.push('ц'),
+            'č' => output.push('ч'),
+            'ć' => output.push('ћ'),
+            'd' => {
+                if let Some(&next) = iter.peek() {
+                    if next == 'ž' {
+                        output.push('џ');
+                        iter.next();
+                    } else {
+                        output.push('д');
                     }
-                    previous_char = a;
+                } else {
+                    output.push('д');
                 }
-
-                'j' => match previous_char {
-                    'l' => {
-                        result.push('љ');
-                        previous_char = '\0';
+            }
+            'đ' => output.push('ђ'),
+            'e' => output.push('е'),
+            'ž' => output.push('ж'),
+            'f' => output.push('ф'),
+            'g' => output.push('г'),
+            'h' => output.push('х'),
+            'i' => output.push('и'),
+            'j' => output.push('ј'),
+            'k' => output.push('к'),
+            'l' => {
+                if let Some(&next) = iter.peek() {
+                    if next == 'j' {
+                        output.push('љ');
+                        iter.next();
+                    } else {
+                        output.push('л');
                     }
-                    'L' => {
-                        result.push('Љ');
-                        previous_char = '\0';
-                    }
-                    'n' => {
-                        result.push('њ');
-                        previous_char = '\0';
-                    }
-                    'N' => {
-                        result.push('Њ');
-                        previous_char = '\0';
-                    }
-                    'd' | 'D' => {
-                        result.push(to_serbian_cyrillic_char(previous_char));
-                        result.push('ј');
-                        previous_char = '\0';
-                    }
-                    '\0' => {
-                        result.push('ј');
-                    }
-                    _ => {
-                        result.push(to_serbian_cyrillic_char(previous_char));
-                        result.push('ј');
-                    }
-                },
-                'ž' => match previous_char {
-                    'd' => {
-                        result.push('џ');
-                        previous_char = '\0';
-                    }
-                    'D' => {
-                        result.push('Џ');
-                        previous_char = '\0';
-                    }
-                    'l' | 'L' | 'n' | 'N' => {
-                        result.push(to_serbian_cyrillic_char(previous_char));
-                        result.push('ж');
-                        previous_char = '\0';
-                    }
-                    '\0' => {
-                        result.push('ж');
-                    }
-                    _ => {
-                        result.push(to_serbian_cyrillic_char(previous_char));
-                        result.push('ж');
-                    }
-                },
-
-                _ => {
-                    if previous_char != '\0' {
-                        result.push(to_serbian_cyrillic_char(previous_char));
-                        previous_char = '\0';
-                    }
-                    result.push(to_serbian_cyrillic_char(a));
+                } else {
+                    output.push('л');
                 }
-            },
-            None => break,
+            }
+            'm' => output.push('м'),
+            'n' => {
+                if let Some(&next) = iter.peek() {
+                    if next == 'j' {
+                        output.push('њ');
+                        iter.next();
+                    } else {
+                        output.push('н');
+                    }
+                } else {
+                    output.push('н');
+                }
+            }
+            'o' => output.push('о'),
+            'p' => output.push('п'),
+            'r' => output.push('р'),
+            's' => output.push('с'),
+            'š' => output.push('ш'),
+            't' => output.push('т'),
+            'u' => output.push('у'),
+            'v' => output.push('в'),
+            'z' => output.push('з'),
+            'A' => output.push('А'),
+            'B' => output.push('Б'),
+            'V' => output.push('В'),
+            'G' => output.push('Г'),
+            'D' => {
+                if let Some(&next) = iter.peek() {
+                    if next == 'ž' {
+                        output.push('Џ');
+                        iter.next();
+                    } else {
+                        output.push('Д');
+                    }
+                } else {
+                    output.push('Д');
+                }
+            }
+            'Đ' => output.push('Ђ'),
+            'E' => output.push('Е'),
+            'Ž' => output.push('Ж'),
+            'Z' => output.push('З'),
+            'I' => output.push('И'),
+            'J' => output.push('Ј'),
+            'K' => output.push('К'),
+            'L' => {
+                if let Some(&next) = iter.peek() {
+                    if next == 'j' {
+                        output.push('Љ');
+                        iter.next();
+                    } else {
+                        output.push('Л');
+                    }
+                } else {
+                    output.push('Л');
+                }
+            }
+            'M' => output.push('М'),
+            'N' => {
+                if let Some(&next) = iter.peek() {
+                    if next == 'j' {
+                        output.push('Њ');
+                        iter.next();
+                    } else {
+                        output.push('Н');
+                    }
+                } else {
+                    output.push('Н');
+                }
+            }
+            'O' => output.push('О'),
+            'P' => output.push('П'),
+            'R' => output.push('Р'),
+            'S' => output.push('С'),
+            'T' => output.push('Т'),
+            'Ć' => output.push('Ћ'),
+            'U' => output.push('У'),
+            'F' => output.push('Ф'),
+            'H' => output.push('Х'),
+            'C' => output.push('Ц'),
+            'Č' => output.push('Ч'),
+            'Š' => output.push('Ш'),
+            _ => output.push(c),
         }
     }
-    result.shrink_to_fit();
 
-    result
+    output
 }
 /// transform utf-8 serbian latinic to serbian cyrilic
 /// does translate two letters nj,lj and dž
-/// and capital Nj,Lj and Dž 
-/// in one cyrilic letter but 
+/// and capital Nj,Lj and Dž
+/// in one cyrilic letter but
 /// in 2 cyrilic letters  
-
-pub fn to_serbian_cyrillic_utf8(word: String) -> String {
-    word
-        .chars()
-        .into_iter()
-        .map(|x| to_serbian_cyrillic_char(x))
-        .collect::<String>()
-}
-
-fn to_serbian_cyrillic_char(letter: char) -> char {
-    match letter {
-        'A' => 'А',
-        'B' => 'Б',
-        'V' => 'В',
-        'G' => 'Г',
-        'D' => 'Д',
-        'Đ' => 'Ђ',
-        'E' => 'Е',
-        'Ž' => 'Ж',
-        'Z' => 'З',
-        'I' => 'И',
-        'J' => 'Ј',
-        'K' => 'К',
-        'L' => 'Л',
-        'ǈ' => 'Љ',
-        'M' => 'М',
-        'N' => 'Н',
-        'ǋ' => 'Њ',
-        'O' => 'О',
-        'P' => 'П',
-        'R' => 'Р',
-        'S' => 'С',
-        'T' => 'Т',
-        'Ć' => 'Ћ',
-        'U' => 'У',
-        'F' => 'Ф',
-        'H' => 'Х',
-        'C' => 'Ц',
-        'Č' => 'Ч',
-        'ǅ' => 'Џ',
-        'Š' => 'Ш',
-
-        'a' => 'а',
-        'b' => 'б',
-        'v' => 'в',
-        'g' => 'г',
-        'd' => 'д',
-        'đ' => 'ђ',
-        'e' => 'е',
-        'ž' => 'ж',
-        'z' => 'з',
-        'i' => 'и',
-        'j' => 'ј',
-        'k' => 'к',
-        'l' => 'л',
-        'ǉ' => 'љ',
-        'm' => 'м',
-        'n' => 'н',
-        'ǌ' => 'њ',
-        'o' => 'о',
-        'p' => 'п',
-        'r' => 'р',
-        's' => 'с',
-        't' => 'т',
-        'ć' => 'ћ',
-        'u' => 'у',
-        'f' => 'ф',
-        'h' => 'х',
-        'c' => 'ц',
-        'č' => 'ч',
-        'ǆ' => 'џ',
-        'š' => 'ш',
-        _ => letter,
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -186,7 +159,7 @@ mod tests {
     #[test]
     fn test_azbuka_small() {
         assert_eq!(
-            to_serbian_cyrillic("abvgdđežzijklljmnnjoprstćufhcčdžš".to_string()),
+            latin_to_cyrillic("abvgdđežzijklljmnnjoprstćufhcčdžš".to_string()),
             "абвгдђежзијклљмнњопрстћуфхцчџш"
         );
     }
@@ -194,14 +167,14 @@ mod tests {
     #[test]
     fn test_azbuka_big() {
         assert_eq!(
-            to_serbian_cyrillic("ABVGDĐEŽZIJKLLjMNNjOPRSTĆUFHCČDžŠ".to_string()),
+            latin_to_cyrillic("ABVGDĐEŽZIJKLLjMNNjOPRSTĆUFHCČDžŠ".to_string()),
             "АБВГДЂЕЖЗИЈКЛЉМНЊОПРСТЋУФХЦЧЏШ"
         );
     }
     #[test]
     fn test_azbuka_small_1000000() {
         assert_eq!(
-            to_serbian_cyrillic(
+            latin_to_cyrillic(
                 "abvgdđežzijklljmnnjoprstćufhcčdžš"
                     .repeat(1000000)
                     .to_string()
@@ -213,7 +186,7 @@ mod tests {
     #[test]
     fn test_azbuka_big_1000000() {
         assert_eq!(
-            to_serbian_cyrillic(
+            latin_to_cyrillic(
                 "ABVGDĐEŽZIJKLLjMNNjOPRSTĆUFHCČDžŠ"
                     .repeat(1000000)
                     .to_string()
@@ -223,23 +196,6 @@ mod tests {
     }
     #[test]
     fn test_njljdž() {
-        assert_eq!(
-            to_serbian_cyrillic(
-                "njljdžNjLjDž"
-                    .to_string()
-            ),
-            "њљџЊЉЏ"
-        );
+        assert_eq!(latin_to_cyrillic("njljdžNjLjDž".to_string()), "њљџЊЉЏ");
     }
-    #[test]
-    fn test_njljdž_utf8() {
-        assert_eq!(
-            to_serbian_cyrillic_utf8(
-                "njljdžNjLjDž"
-                    .to_string()
-            ),
-            "нјлјджНјЛјДж"
-        );
-    }
-
 }
